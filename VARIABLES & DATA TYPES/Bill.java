fc9c9defd425498dba3569109013c2ba496486b3
Palak565 <palakgoyal565@gmail.com>
2024-12-13 15:11:37 +0530

    calculate bill

---
+++ VARIABLES & DATA TYPES/Bill.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Bill {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter costs of pen, pencil and eraser:");
+        float penCost = sc.nextFloat();
+        float pencilCost = sc.nextFloat();
+        float eraserCost = sc.nextFloat();
+        float totalcost = penCost + pencilCost + eraserCost;
+        float tax = 0.18f * totalcost;
+        System.out.println("Total cost w/o tax: Rs" + totalcost);
+        System.out.println("Total cost with tax: Rs" + (totalcost + tax));
+    }
+}
