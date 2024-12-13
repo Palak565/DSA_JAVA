bc613b13a8338ec5c7663e5e12107bd2a7737ad2
Palak565 <palakgoyal565@gmail.com>
2024-12-13 20:46:49 +0530

    Loops module

---
+++ LOOPS/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive number:");
+        int n = sc.nextInt();
+        int fact = 1;
+        if (n < 0){
+            System.out.println("Factorial doesn't exist for negative numbers!");
+        } else {
+            int i = 2;
+            while (i <= n){
+                fact *= i;
+                i++;
+            }
+            System.out.println("Factorial = " + fact);
+        }
+    }
+}
