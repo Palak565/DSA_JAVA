773e276896526e1800b63bfc02cd3db8efb580bd
Palak565 <palakgoyal565@gmail.com>
2024-12-13 19:53:44 +0530

    Conditional statements module

---
+++ CONDITIONAL STATEMENTS/Week.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Week {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter day(1-7):");
+        int day = sc.nextInt();
+        switch (day){
+            default: System.out.println("Wrong day!");
+                    break;
+            case 1: System.out.println("Monday");
+                    break;
+            case 2: System.out.println("Tuesday");
+                    break;
+            case 3: System.out.println("wednesday");
+                    break;
+            case 4: System.out.println("Thursday");
+                    break;
+            case 5: System.out.println("Friday");
+                    break;
+            case 6: System.out.println("Saturday");
+                    break;
+            case 7: System.out.println("Sunday");
+                    break;
+        }
+    }
+}
