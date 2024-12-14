780df9d92c0dbee966d0aaa505e831c1a9b8a8b8
Palak565 <palakgoyal565@gmail.com>
2024-12-14 11:37:07 +0530

    Functions module

---
+++ FUNCTIONS/IsEven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class IsEven {
+    public static boolean isEven(int x){
+        if (x % 2 == 0){
+            return true;
+        } 
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a = sc.nextInt();
+        System.out.println("Is " + a + " even? " + isEven(a));
+    }
+}
---
+++ FUNCTIONS/Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Palindrome {
+    public static boolean isPalindrome(int num) {
+        int rev = 0, n = num;
+        while (n > 0){
+            rev = rev*10 + n%10;
+            n = n/10;
+        }
+        if (rev == num){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a = sc.nextInt();
+        System.out.println("Is " + a + " a palindrome? " + isPalindrome(a));
+    }
+}
